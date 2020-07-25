923bb542d26dd875894521387e72726f2d6ed744
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-07-24 23:31:29 -0300

    Operações aritmeticas

---
+++ opari.py
@@ -0,0 +1,14 @@
+#Codigo para estudo de aritmética basica
+
+a=2+1 #operação de soma
+b=2-1#operação de subtraçao
+c=2*2#operaçao de multiplicaçao
+d=2/2#operação de divisao, divisoes retornam valores float
+e=2**2#operação de elevar ao quadrado
+f=2**0.5#operaçao de raiz quadrada
+g=10%3#resto da divisao (resto)
+h=2+10*10+3#as operaçoes ocorrem igual na matematica 
+i=(2+10)*(10+3)#tambem é possivel utlizar parenteses
+
+
+
