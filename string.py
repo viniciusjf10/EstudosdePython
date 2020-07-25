561d84c4f9a90e2b162f2f6eb9eda188e929a079
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-07-24 23:59:54 -0300

    Estudo de strings

---
+++ string.py
@@ -0,0 +1,15 @@
+#Estudo de strings
+#strings sao escritas usando aspas simples ou aspas duplas
+
+'hello'
+"hello"
+'isso tambem é uma string'
+
+#imprimindo uma string
+
+print("hello world 1")
+print("use \n para inserir uma nova linha")
+
+#verificando o comprimento de uma string
+c=len("hello world")
+print(c)
