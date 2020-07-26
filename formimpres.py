1e3217ddf0385b2852a7e4f34712c44774fe6dea
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-07-25 23:04:46 -0300

    Primeira parte do estudo de formataçao de impressao

---
+++ formimpres.py
@@ -0,0 +1,13 @@
+#Formatação de impressão
+
+print("this is a string")
+s='string'
+print('place another string with a mod and s: %s' %(s))
+
+#Exibiçao de numeros de ponto flutuante 
+print('floatin point numbers: %1.2f' %(13.144))
+
+print('floatin point numbers: %1.5f' %(13.144))
+
+print('floatin point numbers: %10.2f' %(13.144))
+
