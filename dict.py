8290eb8610b5abb1618dc5dae4a5a7261fc90f1c
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-07-26 17:43:04 -0300

    Inicio do estudo de dicionarios

---
+++ dict.py
@@ -0,0 +1,11 @@
+#Estudos sobre dicionarios
+
+my_dict={'key1':'value1','key2':'value2'}
+print(my_dict['key2'])
+#Dicionarios podem receber todo tipo de dados 
+my_dict2={'key1':'1223','key2':'casa','key3':['item0','item1','item3']}
+print(my_dict2['key3'][0])
+
+#tambem é possivel usar metodos de formataçao
+print(my_dict2['key3'][0].upper())
+
