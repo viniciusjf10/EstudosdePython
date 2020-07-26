dd639d8dd11888ef1d5ad7a1e82ceb3f1853e034
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-07-25 23:44:32 -0300

    Segunda parte e exercicios de formatação de impressão

--- formimpres.py
+++ formimpres.py
@@ -11,3 +11,16 @@ print('floatin point numbers: %1.5f' %(13.144))
 
 print('floatin point numbers: %10.2f' %(13.144))
 
+#format()
+
+print('this is a string with an {p}'.format(p='insert'))
+
+print('Object1: {a}, object2: {b}'.format(a=1,b='two'))
+
+#Exercicios
+
+print('o diamentro do planeta {planeta} é de {diametro}'.format(planeta="Terra", diametro=12742))
+
+
+
+
