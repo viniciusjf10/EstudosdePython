6fe1105e2540d9185e7612e16b4bd15cbd4964c1
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-07-28 23:22:27 -0300

    Exercicios else if elif

---
+++ elseifelif.py
@@ -0,0 +1,29 @@
+#if else elif
+#if = se; else = se nao ; elif = se nao se
+
+x='camis'
+if x=='casa':
+    print('X é casa')
+elif x=='camisa':
+    print('X é camisa')
+else:
+    print('Nao faço ideia')
+    
+#Exercicios
+a=3
+b=6
+c=9
+
+if(a+b<c):
+    print('não é triangulo')
+elif(a+c<b):
+    print('não é triangulo')
+elif(b+c<a):
+    print('não é triangulo')
+else:
+    print('é triangulo')
+    
+
+    
+    
+    
