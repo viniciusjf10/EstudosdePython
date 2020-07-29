cb7a13f1ad038942aeae60ce48df608c854a3f97
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-07-28 22:58:58 -0300

    Estudo de comparaçao de variaveis

---
+++ comparativo.py
@@ -0,0 +1,23 @@
+#Estudos de operadores de comparaçao
+
+a=1
+b=2
+x=(a==b)
+print(x)
+x=(a!=b)
+print(x)
+x=(a>b)
+print(x)
+x=(a<b)
+print(x)
+x=(a>=b)
+print(x)
+x=(a<=b)
+print(x)
+
+#Operadores em cadeia
+1<2<3 #verifica se 1 é menor que 2 e se 2 é menor que 3
+1<2 and 2<3 #outra escrita para expressao acima
+
+#exercicios
+
