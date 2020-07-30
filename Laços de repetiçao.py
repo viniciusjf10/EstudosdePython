49a047a1bfe3ae70a3f8bf89cec38f8315d08309
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-07-30 20:43:13 -0300

    Segunda parte do estudo de for

--- Laços de repetiçao.py
+++ Laços de repetiçao.py
@@ -37,4 +37,7 @@ l=[1,4,5,67,7,8,6,54,4,3,1,21,1,9,0]
 for num in l:
     if num %2 ==0:
         print(num)
-        
+print('----------------')         
+stri='Hello Python'
+for i in stri[6:]:
+    print(i)
