e0a9aeaca8c33354cc9dea4a6a6859b70784aeea
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-07-30 20:18:48 -0300

    Estudo de for

---
+++ Laços de repetiçao.py
@@ -0,0 +1,33 @@
+#Estudo de laços de repetição
+
+l=[1,2,3,4,5,6,7,8]
+for num in l:
+        print(num)
+print('----------------')
+for num in l:
+    if num % 2 == 0:
+        print(num)
+print('----------------')
+for num in l:
+    if num % 2 ==0:
+        print(num)
+    else:
+        print('num impar')
+print('----------------')
+for letter in 'This is a string':
+    print(letter)
+print('----------------') 
+tup=(1,2,3,4,5)
+for t in tup:
+    print(t)
+print('----------------') 
+l=[(2,4),(6,8),(10,12)]
+for tup in  l:
+    print(tup)
+print
+print('----------------') 
+d = {'k1':1, 'k2':2, 'k3':3}
+for k, v in d.itens()
+    print(k)
+    print(v)      
+print('----------------') 
