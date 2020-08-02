bacf9f4e673901c81643a98ccf8893aa72b244d0
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-02 17:26:55 -0300

    Exercicios while

--- while.py
+++ while.py
@@ -20,10 +20,20 @@ while x<10:
     print('x is currently: ',x)
     print(' x is still less than 10, adding 1 to x')
     x+=1
-    if(x==3)
+    if x==3:
         print('x==3')
     else:
         print('continuing')
         continue
+
+#Exercicios
+
+x=0
+while x<=4:
+    print('*'*x)
+    x+=1
+while x>0:
+    print('*'*x)
+    x-=1
     
         
