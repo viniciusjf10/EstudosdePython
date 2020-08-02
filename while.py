f782704673788f0e7fee7cba219108477f2e950f
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-02 16:43:52 -0300

    Estudo de while parte 2

--- while.py
+++ while.py
@@ -14,4 +14,16 @@ while x<10:
 else:
     print('Well done')
 
+#break, continuos, pass
+x=0
+while x<10:
+    print('x is currently: ',x)
+    print(' x is still less than 10, adding 1 to x')
+    x+=1
+    if(x==3)
+        print('x==3')
+    else:
+        print('continuing')
+        continue
+    
         
