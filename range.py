e053ad0928ead6ab552c652df46795fae7602eb5
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-08 17:35:32 -0300

    Estudo range

--- range.py
+++ range.py
@@ -1,1 +1,15 @@
 #Estudos de range
+#range gera numeros dentro de um intervalo
+
+r=range(0,10)
+print(r) #nao exibe lista
+print(tuple(r))  #exibe lista
+
+r=(range(0,20,2)) #temos um range de 0 a 20 com intervalo de 2 em 2
+
+for i in range(0,10):
+    print(i)
+
+    
+#Exercicios
+
