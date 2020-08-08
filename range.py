0577d2e84cd499b6b6146bc39395190e3e59aba9
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-08 17:44:50 -0300

    Exercicios de range

--- range.py
+++ range.py
@@ -12,4 +12,12 @@ for i in range(0,10):
 
     
 #Exercicios
-
+for i in range (1,51):
+    if i%3==0 and i%5==0:
+        print(i, "-FizzBuzz")
+    elif i%3==0:
+        print(i, "-buzz")
+    elif i%5==0:
+        print(i, "-Buzz")
+        
+        
