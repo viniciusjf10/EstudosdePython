75ac08d4129034cd3ae3d9f19e5dfb40d12ab72f
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-08 23:31:36 -0300

    Estudo de function

---
+++ function.py
@@ -0,0 +1,21 @@
+#function
+#fazer funcões é util quando um trecho de código se repete varias vezes 
+def name_of_function (arg1,arg2):
+    '''
+    #documentaçao da funçao
+
+    Parameters
+    ----------
+    arg1 : TYPE
+        DESCRIPTION.
+    arg2 : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    '''
+    #faça a implementaçao aqui pra baixo
+    
+    return #coloque aqui o retorno da função
