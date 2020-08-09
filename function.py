e632923ac4dc5e3e9188327d93ca229c1963d9ac
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-08 23:40:44 -0300

    Estudo de function2

--- function.py
+++ function.py
@@ -19,3 +19,40 @@ def name_of_function (arg1,arg2):
     #faça a implementaçao aqui pra baixo
     
     return #coloque aqui o retorno da função
+
+def say_hello():
+    print("Hello")
+    
+say_hello()
+
+def greeting(name):
+    print('hello, %s' %name)
+greeting('Vinicius')
+
+#usando return
+
+def add_num(num1,num2):
+    return num1+num2
+print(add_num(1,5))
+
+def is_primo(num):
+    for n in range (2,num):
+        if num %2 ==0:
+            print("nao é primo")
+            break
+        else:
+            print('é primo')
+            
+is_primo(18)
+
+import math
+def is_prime(num):
+    if num %2 ==0 and num >2:
+        return false
+    for i in range (3,int(math.sqrt(num))):
+        if num %i==0:
+            return false
+    return True
+
+print(is_prime(11))
+    
