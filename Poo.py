891a413ef7729ebda7dd6c4ac3d8d37792165121
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-09 23:32:28 -0300

    poo

--- Poo.py
+++ Poo.py
@@ -21,3 +21,24 @@ frank = Dog(breed='Huskie')
 print(sam.breed)
 print(frank.breed)
 
+#criando um atributo especial para a classe
+
+class Dog(object):
+    species = "mamal"
+    def __init__(self,breed,name):
+         self.breed=breed
+         self.name=name
+sam=Dog('Lab','Sam')
+print(sam.name)
+
+class Circle (object):
+    def __init__(self,radius=1): #O raio padrao para o circulo é 1
+        self.radius = radius 
+    def area(self):
+        return self.radius *self.radius *Circle.pi
+    def serRadius(self,radius):
+        self.radius=radius
+    def getRadius(self):
+        return self.radius
+    
+        
