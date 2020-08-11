c6f09630fd0a7fb77ce069e8a5e921988d27c45d
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-11 02:25:07 -0300

    Exercicios de pacotes

--- modulos.py
+++ modulos.py
@@ -3,4 +3,11 @@ import math
 print(math.ceil(2.4)) #arredonda
 
 print(dir(math)) #exibe todas funçoes que podemos usar
-help(math.ceil)
+help(math.ceil) #documentaçao da funcçao
+#é possivel criar modulos, apenas usando o metodo import "arquivo"
+
+#Exercicos
+#01
+print(math.log10(12))
+#02
+print(math.sqrt(math.pi)) 
