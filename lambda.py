c40f2a35e703adcd392f6e8c4db4a6f9a7f096f8
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-12 23:11:22 -0300

    Estudo de funçao lambda

---
+++ lambda.py
@@ -0,0 +1,30 @@
+#Estudo de funçoes lambda
+#funçoes lambda tem o intuito de criar funçoes anonimas e assim podemos criar
+#funçoes ad hoc sem usar o def
+#usa-se apenas para terefas simples
+
+def square(num):
+    result = num**2
+    return result
+print(square(2))
+
+def square1(num):
+    return num**2
+print(square1(2))
+
+def square3(num): return num**2
+print(square3(2))
+
+square=lambda num:num**2
+print(square(2))
+
+
+even=lambda x:x%2==0
+print(even(3))
+print(even(2))
+
+first=lambda s:s[0]
+print(first('hello'))
+
+rev= lambda s:s[::-1]
+print(rev('casa'))
