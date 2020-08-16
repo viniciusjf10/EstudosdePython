da89002352ac702c41d42421242e7a8a0f018531
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-15 23:27:25 -0300

    Exercicio filter

--- filter.py
+++ filter.py
@@ -9,3 +9,11 @@ lista1=[1,4,9,16,25]
 lst=range(0,11)
 print(list(filter(even_check,lista1)))
 print(list(filter(lambda x:x%2==0,lst)))
+
+#exercicios 
+def primo(n):
+    for i in range(2,n):
+        if n %i==0:
+            return False
+    return n>1
+print(list(filter(primo,range(0,101))))
