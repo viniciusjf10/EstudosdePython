c4b472d21b03608dc994382039f13213af853039
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-15 23:56:59 -0300

    Exercicio zip

--- zip.py
+++ zip.py
@@ -12,4 +12,11 @@ x=[1,2,3,4,5]
 y=[1,2]
 print(list(zip(x,y)))
 
-#exercicios
+#exercicios]
+
+print(list(zip(range(10),[i**2 for i in range(10)])))
+
+l1=list(range(1,11))
+l2=list(range(10,0,-1))
+for i1,i2 in zip (l1,l2):
+    print(i1/i2)
