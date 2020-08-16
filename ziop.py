5242bad97bdbd94c328c62017dc3f8b1027bdb89
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-15 23:49:37 -0300

    Estudo zip

---
+++ ziop.py
@@ -0,0 +1,15 @@
+#Zip
+#cria um iterador que agrega elementos 
+#retorna um iterador de tuplas que contem o n elemento de cada uma das sequencias
+
+x=[1,2,3]
+y=[4,5,6]
+print(list(zip(x,y)))
+
+#nao se aconselha iterar listas de tamanhos diferentes
+
+x=[1,2,3,4,5]
+y=[1,2]
+print(list(zip(x,y)))
+
+#exercicios
