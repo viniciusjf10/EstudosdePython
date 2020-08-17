d6a453c5a6a2c43806861ffcabc1d3ef25820f11
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-16 23:22:54 -0300

    Estudo de any e all

---
+++ anyall.py
@@ -0,0 +1,8 @@
+# Estudo de all e any
+#funçoes que dizem se todos elementos sao true ou ao menos 1 é true
+
+lst=[True, True, False, True]
+print(all(lst)) #ao menos um é falso
+print(any(lst)) #ao menos um é true
+
+#exercicio
