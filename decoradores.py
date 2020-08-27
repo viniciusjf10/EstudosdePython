16f4846cbacdcf0a2420dead4d86114d39019ab8
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-27 20:17:05 -0300

    decod

--- decoradores.py
+++ decoradores.py
@@ -5,10 +5,17 @@ def hello (name="Rodrigo"):
     def welcome ():
         return '\t This is inside the welcome() function'
     if name == "Rodrigo":
-        return greet()
+        return greet
     else:
-        welcome()
+        welcome
 
 print(hello())
 
 
+def hello ():
+    return "Hi Jose!"
+def other (func):
+    print('Other code would go here')
+    print(func())
+    
+print(other(hello))
