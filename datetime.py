0250c6018c54cd3576911130f3bdcb0bd166bdde
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-27 20:53:00 -0300

    time

--- datetime.py
+++ datetime.py
@@ -5,4 +5,16 @@ t=datetime.time(4,20,1) #mostra a diferença entre os componentes
 print(t)
 
 print('Hour:',t.hour)
+print('Minutos:',t.minute)
+print('Segundos:',t.second)
+print('Microsegundos:',t.microsecond)
+
+#Dates
+
+today = datetime.date.today()
+print(today)
+print('ctime',today.ctime())
+print('tuple',today.timetuple())
+print('ordinal',today.toordinal())
+print('year:',today.year)
 
