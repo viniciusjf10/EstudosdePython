f4a0ba556d870575e0d000d0a454496f6f732623
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-30 00:04:30 -0300

    nume

--- numerosavança.py
+++ numerosavança.py
@@ -17,5 +17,12 @@ for i in range (0,33):
     print(bin(i))
     
 def rgb_to_hex(tup):
-    if type(tup) != tuple or any([not (i>=0 and i <256)for i in tup]) or len(tup) !3:
-        raise Exception('Argument ivalid')
+    if type(tup) != tuple or any([not (i>=0 and i <256)for i in tup]) or len(tup) !=3:
+        raise Exception('Argument invalid')
+    hex_code="#"
+    for i in tup:
+        hex_code += hex(round(i))[2:].zfill(2).upper()
+    return hex_code
+
+print(rgb_to_hex((255,255,255)))
+print(rgb_to_hex((69,140,15)))
