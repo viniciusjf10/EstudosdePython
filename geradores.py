b3f2180f99d15db5c06d3079aae11a4c123042ad
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-30 23:08:44 -0300

    geradores

--- geradores.py
+++ geradores.py
@@ -2,4 +2,9 @@
 #sao funçoes que sao suspensas e retomam a execução no ultimo ponto gerando uma 
 #suspensao de estado
 
+def gencuber(n):
+    for num in range(n):
+        yield num **3
+for x in gencuber(10):
+    print(x)
 
