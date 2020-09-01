a2e519e766703f08d14eab95157f70b61c3a8307
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-08-31 22:17:56 -0300

    colection

--- coleçoespt1.py
+++ coleçoespt1.py
@@ -6,3 +6,14 @@ l=[1,2,2,2,3,5,5,4,2,1,6,7]
 print(Counter(l))
 
 print(Counter('aaaaaaadddddeufbhewnufjenfuiehnu'))
+
+#defaultdict
+
+from collections import defaultdict
+
+#d={}
+#d['one']
+d=defaultdict(object)
+d['one']
+for item in d:
+    print(item)
